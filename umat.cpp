52023525f58ac1a48ebeb4344d819e5a42f12aa6
ShawnWolgu <garaireen@sjtu.edu.cn>
2024-08-01 23:24:06 +0800

    finish disvel model fundamentally, abaqus version.

--- umat.cpp
+++ umat.cpp
@@ -41,12 +41,12 @@ extern "C" void umat(double* stress, double* statev, double* ddsdde, double* sse
     if (*kstep == 1 && *kinc == 0){
         // Initialize the state variables
 
-        /* char temp[200]; */
-        /* int lenoutdir; */
-        /* getoutdir(temp, &lenoutdir, 200); */
-        /* processPath(temp, "\\param.txt"); */
-        /* ifstream cij(temp); */
-        ifstream cij("param.txt");
+        char temp[200];
+        int lenoutdir;
+        getoutdir(temp, &lenoutdir, 200);
+        processPath(temp, "\\param.txt");
+        ifstream cij(temp);
+        /* ifstream cij("param.txt"); */
         statev[0] = 0;
         statev[1] = 0;
         statev[2] = 0;
@@ -158,86 +158,86 @@ extern "C" void umat(double* stress, double* statev, double* ddsdde, double* sse
     ddsdde_from_matrix(C_ijkl, ddsdde);
 }
 
-int main(){
-    // Initialize the variables
-    double stress[6] = {0};
-    double stress_backup[6] = {0};
-    double statev[100] = {0};
-    double statev_backup[100] = {0};
-    double ddsdde[36] = {0};
-    double sse = 0, spd = 0, scd = 0, rpl = 0, ddsddt = 0, drplde = 0, drpldt = 0;
-    double stran[6] = {0}, dstran[6] = {0};
-    double predef = 0, dpred = 0;
-    char cmname[1] = {'\0'};
-    int ndi = 0, nshr = 0, ntens = 0, nstatv = 13, nprops = 0;
-    double props[4] = {10, 10, 10, 0.5};
-    double coords[3] = {0}, drot[9] = {0}, pnewdt = 0, celent = 0, dfgrd0[3] = {0}, dfgrd1[3] = {0};
-    int noel = 0, npt = 0, layer = 0, kspt = 0, kstep = 1, kinc = 0;
-    short cmname_len = 0;
-
-    // Define the deformation rate
-    double deformation_rate = 0.01;  // This is just an example value
-
-    // Define the time step
-    double time = 0;
-    double dtime = 0.01;  // This is just an example value
-    // Define the temperature 
-    double temp = 300, dtemp = 0;
-    //
-    // Time-stepping loop
-    for (int kinc = 0; kinc < 1000; ++kinc) {
-        // Initialize the strain increment
-        dstran[0] = deformation_rate * dtime;
-        dstran[1] = -deformation_rate * dtime * 0.4866;
-        dstran[2] = -deformation_rate * dtime * 0.4866;
-        dstran[3] = 0;
-        dstran[4] = 0;
-        dstran[5] = 0;
-
-        Eigen::MatrixXd F(5, 1); F = Eigen::MatrixXd::Zero(5, 1);
-        Eigen::MatrixXd dF(5, 5); dF = Eigen::MatrixXd::Zero(5, 5);
-
-        for (int n_iter = 0; n_iter < 20; n_iter++){
-            // Call the umat function
-            for (int i = 0; i < 100; i++){
-                statev[i] = statev_backup[i];
-            }
-            for (int i = 0; i < 6; i++){
-                stress[i] = stress_backup[i];
-            }
-            umat(stress, statev, ddsdde, &sse, &spd, &scd, &rpl, &ddsddt, &drplde, &drpldt,
-                 stran, dstran, &time, &dtime, &temp, &dtemp, &predef, &dpred, cmname, &ndi,
-                 &nshr, &ntens, &nstatv, props, &nprops, coords, drot, &pnewdt, &celent, dfgrd0,
-                 dfgrd1, &noel, &npt, &layer, &kspt, &kstep, &kinc, cmname_len);
-            for (int i = 1; i < 6; i++){
-                F(i-1) = stress[i];
-                for (int j = 1; j < 6; j++){
-                    dF(i-1, j-1) = ddsdde[6*i + j];
-                }
-            }
-            if (F.norm() < 0.1) break;
-            Eigen::MatrixXd dX(5,1);
-            dX = dF.inverse() * F;
-            for (int i = 0; i < 5; i++){
-                dstran[i+1] -= 1* dX(i);
-            }
-        }
-        for (int i = 0; i < 100; i++){
-            statev_backup[i] = statev[i];
-        }
-        for (int i = 0; i < 6; i++){
-            stress_backup[i] = stress[i];
-        }
-        for (int i = 0; i < 6; i++){
-            stran[i] += dstran[i];
-        }
-        // Print the results for this step
-        std::cout << stran[0] << "," << stran[1] << "," << stran[2] << "," << stran[3] << "," << stran[4] << "," << stran[5] << ",";
-        std::cout << stress[0] << "," << stress[1] << "," << stress[2] << "," << stress[3] << "," << stress[4] << "," << stress[5] << ",";
-        std::cout << statev[0] << "," << statev[1] << "," << statev[2] << "," ;
-        std::cout << statev[sdv_ind(0,"DD")] << "," << statev[sdv_ind(1,"DD")] << "," << statev[sdv_ind(2,"DD")] << "," << statev[sdv_ind(3,"DD")] << ",";
-        std::cout << statev[sdv_ind(4,"DD")] << "," << statev[sdv_ind(5,"DD")] << "," << statev[sdv_ind(6,"DD")] << "," << statev[sdv_ind(7,"DD")] << ",";
-        std::cout << statev[sdv_ind(8,"DD")] << "," << statev[sdv_ind(9,"DD")] << "," << statev[sdv_ind(10,"DD")] << "," << statev[sdv_ind(11,"DD")] << endl;
-    }
-    return 0;
-}
+/* int main(){ */
+/*     // Initialize the variables */
+/*     double stress[6] = {0}; */
+/*     double stress_backup[6] = {0}; */
+/*     double statev[100] = {0}; */
+/*     double statev_backup[100] = {0}; */
+/*     double ddsdde[36] = {0}; */
+/*     double sse = 0, spd = 0, scd = 0, rpl = 0, ddsddt = 0, drplde = 0, drpldt = 0; */
+/*     double stran[6] = {0}, dstran[6] = {0}; */
+/*     double predef = 0, dpred = 0; */
+/*     char cmname[1] = {'\0'}; */
+/*     int ndi = 0, nshr = 0, ntens = 0, nstatv = 13, nprops = 0; */
+/*     double props[4] = {10, 10, 10, 0.5}; */
+/*     double coords[3] = {0}, drot[9] = {0}, pnewdt = 0, celent = 0, dfgrd0[3] = {0}, dfgrd1[3] = {0}; */
+/*     int noel = 0, npt = 0, layer = 0, kspt = 0, kstep = 1, kinc = 0; */
+/*     short cmname_len = 0; */
+/**/
+/*     // Define the deformation rate */
+/*     double deformation_rate = 0.01;  // This is just an example value */
+/**/
+/*     // Define the time step */
+/*     double time = 0; */
+/*     double dtime = 0.01;  // This is just an example value */
+/*     // Define the temperature  */
+/*     double temp = 300, dtemp = 0; */
+/*     // */
+/*     // Time-stepping loop */
+/*     for (int kinc = 0; kinc < 1000; ++kinc) { */
+/*         // Initialize the strain increment */
+/*         dstran[0] = deformation_rate * dtime; */
+/*         dstran[1] = -deformation_rate * dtime * 0.4866; */
+/*         dstran[2] = -deformation_rate * dtime * 0.4866; */
+/*         dstran[3] = 0; */
+/*         dstran[4] = 0; */
+/*         dstran[5] = 0; */
+/**/
+/*         Eigen::MatrixXd F(5, 1); F = Eigen::MatrixXd::Zero(5, 1); */
+/*         Eigen::MatrixXd dF(5, 5); dF = Eigen::MatrixXd::Zero(5, 5); */
+/**/
+/*         for (int n_iter = 0; n_iter < 20; n_iter++){ */
+/*             // Call the umat function */
+/*             for (int i = 0; i < 100; i++){ */
+/*                 statev[i] = statev_backup[i]; */
+/*             } */
+/*             for (int i = 0; i < 6; i++){ */
+/*                 stress[i] = stress_backup[i]; */
+/*             } */
+/*             umat(stress, statev, ddsdde, &sse, &spd, &scd, &rpl, &ddsddt, &drplde, &drpldt, */
+/*                  stran, dstran, &time, &dtime, &temp, &dtemp, &predef, &dpred, cmname, &ndi, */
+/*                  &nshr, &ntens, &nstatv, props, &nprops, coords, drot, &pnewdt, &celent, dfgrd0, */
+/*                  dfgrd1, &noel, &npt, &layer, &kspt, &kstep, &kinc, cmname_len); */
+/*             for (int i = 1; i < 6; i++){ */
+/*                 F(i-1) = stress[i]; */
+/*                 for (int j = 1; j < 6; j++){ */
+/*                     dF(i-1, j-1) = ddsdde[6*i + j]; */
+/*                 } */
+/*             } */
+/*             if (F.norm() < 0.1) break; */
+/*             Eigen::MatrixXd dX(5,1); */
+/*             dX = dF.inverse() * F; */
+/*             for (int i = 0; i < 5; i++){ */
+/*                 dstran[i+1] -= 1* dX(i); */
+/*             } */
+/*         } */
+/*         for (int i = 0; i < 100; i++){ */
+/*             statev_backup[i] = statev[i]; */
+/*         } */
+/*         for (int i = 0; i < 6; i++){ */
+/*             stress_backup[i] = stress[i]; */
+/*         } */
+/*         for (int i = 0; i < 6; i++){ */
+/*             stran[i] += dstran[i]; */
+/*         } */
+/*         // Print the results for this step */
+/*         std::cout << stran[0] << "," << stran[1] << "," << stran[2] << "," << stran[3] << "," << stran[4] << "," << stran[5] << ","; */
+/*         std::cout << stress[0] << "," << stress[1] << "," << stress[2] << "," << stress[3] << "," << stress[4] << "," << stress[5] << ","; */
+/*         std::cout << statev[0] << "," << statev[1] << "," << statev[2] << "," ; */
+/*         std::cout << statev[sdv_ind(0,"DD")] << "," << statev[sdv_ind(1,"DD")] << "," << statev[sdv_ind(2,"DD")] << "," << statev[sdv_ind(3,"DD")] << ","; */
+/*         std::cout << statev[sdv_ind(4,"DD")] << "," << statev[sdv_ind(5,"DD")] << "," << statev[sdv_ind(6,"DD")] << "," << statev[sdv_ind(7,"DD")] << ","; */
+/*         std::cout << statev[sdv_ind(8,"DD")] << "," << statev[sdv_ind(9,"DD")] << "," << statev[sdv_ind(10,"DD")] << "," << statev[sdv_ind(11,"DD")] << endl; */
+/*     } */
+/*     return 0; */
+/* } */
